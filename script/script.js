6fcaf9f717815566455543d29b959dbae2831538
TeerachanonDev <94124435+TeerachanonDev@users.noreply.github.com>
2022-09-26 16:16:11 +0700

    new

---
+++ script/script.js
@@ -0,0 +1,13 @@
+Swal.fire({
+    title: 'ยินดีต้อนรับ',
+    text: 'นี่คือเว็บไซต์เก็บผลงานของ นายธีรชานนท์ พวงเงิน',
+    
+  })
+
+function btn1(){
+    Swal.fire({        
+        imageUrl: "image/01 (1).jpg",
+        imageWidth: 505,
+        imageHeight: 670,
+      })
+}
