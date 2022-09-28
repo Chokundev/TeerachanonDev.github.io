6d985adc5e80694296359ea890714d129efe0757
TeerachanonDev <94124435+TeerachanonDev@users.noreply.github.com>
2022-09-28 20:25:03 +0700

    new button

--- script/script.js
+++ script/script.js
@@ -1,8 +1,25 @@
+let timerInterval
 Swal.fire({
-    title: 'ยินดีต้อนรับ',
-    text: 'นี่คือเว็บไซต์เก็บผลงานของ นายธีรชานนท์ พวงเงิน',
-    
-  })
+  title: 'ระบบกำลังโหลดข้อมูล',
+  html: 'จะโหลดเสร็จภายใน <b></b> milliseconds.',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
 
 function btn1(){
     Swal.fire({        
@@ -15,7 +32,79 @@ function btn1(){
 function btn2(){
     Swal.fire({        
         imageUrl: "image/02.png",
-        imageWidth: 670,
-        imageHeight: 505,
+        imageWidth: 750,
+        imageHeight: 400,
       })
+}
+
+function btn3(){
+  Swal.fire({        
+      imageUrl: "image/01 (2).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn4(){
+  Swal.fire({        
+      imageUrl: "image/01 (10).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn5(){
+  Swal.fire({        
+      imageUrl: "image/01 (11).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn6(){
+  Swal.fire({        
+      imageUrl: "image/01 (12).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn7(){
+  Swal.fire({        
+      imageUrl: "image/01 (13).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn8(){
+  Swal.fire({        
+      imageUrl: "image/01 (14).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn9(){
+  Swal.fire({        
+      imageUrl: "image/01 (15).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn10(){
+  Swal.fire({        
+      imageUrl: "image/01 (16).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
+}
+
+function btn11(){
+  Swal.fire({        
+      imageUrl: "image/01 (17).jpg",
+      imageWidth: 750,
+      imageHeight: 400,
+    })
 }
